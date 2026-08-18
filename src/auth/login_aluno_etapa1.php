131416eec3d954fa1155e36b65e41f4a79069216
joao assis <jg.morais09@gmail.com>
2026-08-18 16:20:12 -0300

    Adiciona etapa 1 do login/sign up  de aluno

---
+++ src/auth/login_aluno_etapa1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>FitControl</h1>
+        <h1>Bem-vindo ao FitControl. Acesse seus treinos e evolução.</h1>
+    </div>
+    <div>
+        <h1>Inscreva-se no FitControl</h1>
+        <div>
+            <label for="email">Email*</label><br>
+            <input type="email" id="email" placeholder="E-mail"><br><br>
+
+            <label for="senha">Senha*</label><br>
+            <input type="password" id="senha" placeholder="Senha"><br><br>
+
+            <label for="nome-usuario">Nome de usuário*</label><br>
+            <input type="text" id="nome-usuario" placeholder="Nome de usuário"><br><br>
+
+            <label for="cpf">CPF</label><br>
+            <input type="text" id="cpf" placeholder="123.456.789.10"><br><br>
+
+            <button for="butao-criar-conta">Enviar Dados</button>
+
+        </div>
+    </div>
+</body>
+</html>
