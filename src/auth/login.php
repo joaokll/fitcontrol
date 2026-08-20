bd790a02143f1d996431f630bec78076f4905c77
bluecatcom <262645688+bluecatcom@users.noreply.github.com>
2026-08-19 21:08:45 -0300

    Banco de dados criado, login melhorado, registrar.php criado, autenticar.php criado e parcialmente feito, conexão.php configurado com pdo

--- src/auth/login.php
+++ src/auth/login.php
@@ -1,70 +1,35 @@
+<?php
+
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>login</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
 </head>
 <body>
+    <form action="autenticar.php" method="POST">
+        
+        <div>    
+            <label>
+            Email: 
+            </label>
+            <input type="email" name="email" required> <br> </br>
+
+            <label>
+            Senha: 
+            </label>
+            <input type="password" name="password" required> <br> </br>
+
+            <button type="subtmit">
+            Entrar
+            </button> <br> </br>
+        </div>
     
-<div class="container1"> 
-    <form method="GET" class="form-login">
-
-        <label for="entrar" class="form-label">
-            Escolha a opção pra entrar:
-        </label>
-
-        <select name="entrar" id="entrar" class="form-select">
-            <option value="login">Login</option>
-            <option value="registrar">Registrar</option>
-        </select>
-
-        <button type="submit" class="form-button">
-            Confirmar
-        </button>
-
     </form>
-</div>
-
-    <?php
-
-    $entrarOpcao = $_GET['entrar'] ?? '';
-
-    switch ($entrarOpcao) {
-        case 'login':
-            $botao = 'Login';
-            break;
-        case 'registrar':
-            $botao = 'Registrar';
-            break;
-        default:
-            $botao = null;
-    }
-    ?>
-
-    <?php if ($botao) : ?>
-    <div class="container1">
-        <form method="POST">
-            <label for="usuario">Usuário:</label>
-            <input type="text" name="usuario" id="usuario"> <br>
-
-            <label for="senha">Senha:</label>
-            <input type="password" name="senha" id="senha"> <br>
-
-            <button type="submit"><?= $botao ?></button>
-        </form>
-    </div>
-    <?php endif; ?>
-
-    <?php
-
-    $usuario = $_POST['usuario'] ?? '';
-    $senha = $_POST['senha'] ?? '';
-
-    // Teste pra ver se ta pegando
-    echo" seu usuario é: {$usuario} e a senha é: {$senha}";
-    ?>
-
 </body>
 </html>
