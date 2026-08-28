e42379a6a117073607b45c2e2d15ee96665bc958
bluecatcom <262645688+bluecatcom@users.noreply.github.com>
2026-08-28 15:21:56 -0300

    conexao.php ajustes de erros

--- src/config/conexao.php
+++ src/config/conexao.php
@@ -5,11 +5,12 @@ $dbname = 'fitcontrol';
 $username = 'root';
 $password = '';
 
+$charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
 
 try {
-    $pdo = new PDO($dsn, $user, $pass);
+    $pdo = new PDO($dsn, $username, $password);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
