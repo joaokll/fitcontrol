b906eb7aea825919a731b3d5b9ac13e91c148254
BlueCatCom <262645688+bluecatcom@users.noreply.github.com>
2026-09-01 18:20:18 +0000

    Ajuste de erro Logout

--- autenticacao/logout.php
+++ autenticacao/logout.php
@@ -3,5 +3,5 @@
 require_once __DIR__ . '/../database/config.php';
 session_unset();
 session_destroy();
-header('Location: login.php');
+header('Location: /fitcontrol-master/autenticacao/login.php');
 exit;
